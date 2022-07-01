6ed18213f814072a74fe7809e8b80beb46a1259a
Aleksi <aleksi.hasu@tuni.fi>
2022-07-02 00:08:02 +0300

    Update footer

--- app.js
+++ app.js
@@ -55,3 +55,96 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+
+
+class Popup {
+  classPopupShow =  'popup--show';
+  classForBody = 'popup-active';
+  popup = null;
+  closeOnClickOverlay = false;
+
+  constructor(options = {}) {
+
+    this.closeOnClickOverlay = options?.closeOnClickOverlay || false;
+   
+    // check if page has vertical scroll
+    this.scrollWidthInit();
+
+    document.addEventListener('click', e => {
+      
+      // open
+      if (e.target.dataset.popupOpen) {
+        e.preventDefault();
+        this.popup = document.getElementById(e.target.dataset.popupOpen);
+        this.show();
+      }
+
+      // close
+      if (e.target.hasAttribute('data-popup-close') ||
+         (e.target.closest('.popup__close') && e.target.closest('.popup__close').hasAttribute('data-popup-close')) ||
+         (this.closeOnClickOverlay && e.target.classList.contains('popup__content'))
+      ){
+        e.preventDefault();
+        this.hide();
+      }
+    })
+  }
+
+  scrollWidthInit() {
+
+    function existVerticalScroll() {
+      let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0),
+          sh =  document.body.scrollHeight;
+
+      return sh > vh ? true : false;
+    }
+
+    let div = document.createElement('div');
+
+    div.style.overflowY = 'scroll';
+    div.style.width = '50px';
+    div.style.height = '50px';
+
+    document.body.append(div);
+
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    if (existVerticalScroll()) {
+      document.documentElement.style.setProperty('--scroll-width', scrollWidth + 'px');
+    }
+  }
+
+  show() {
+    
+    if (this.popup) {
+      this.popup.classList.add(this.classPopupShow);
+    }
+
+    document.body.classList.add(this.classForBody);
+  }
+
+  hide() {
+
+    if (this.popup) {
+      this.popup.classList.remove(this.classPopupShow);
+    }
+
+    document.body.classList.remove(this.classForBody);
+  }
+}
+
+new Popup({
+  closeOnClickOverlay: true, // if you want popup to be closed after click on overlay (default: false)
+});
+
+
+var details = document.querySelector("details")
+
+
+
+details.addEventListener("toggle", function() {
+  if(details.hasAttribute("open")) {
+  }
+})
